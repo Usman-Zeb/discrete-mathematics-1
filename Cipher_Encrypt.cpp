3a04ccefbb372b808a058a2dfccda54cc837d458
Usman-Zeb <58597745+Usman-Zeb@users.noreply.github.com>
2019-12-13 00:45:02 +0500

    Update Cipher_Encrypt.cpp
    Converted to class

--- Cipher_Encrypt.cpp
+++ Cipher_Encrypt.cpp
@@ -6,98 +6,116 @@
 using namespace std;
 
 
-void encrypt()
+class encrypted
 {
-	ifstream input;
-	input.open("message.txt");
-	ofstream output;
-	output.open("encrypted.txt");
-	int k = 0;
-	int shift = 0;
-	char temp;
-	string reference = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789 \n,.";
+private:
+	int k;
+	string reference;
 
-	cout << "Enter a non-negative value for k: ";																
-	cin >> k;
-	
-	if (k >= 0)
+public:
+
+	encrypted()
 	{
-		while (input.get(temp))
-		{
+		k = 0;
+		reference = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789 \n,.";
+	}
 
-			for (int j = 0; j < reference.length(); j++)
+	void encrypt()
+	{
+		ifstream input;
+		input.open("message.txt");
+		ofstream output;
+		output.open("encrypted.txt");
+		int shift = 0;
+		char temp;
+
+		cout << "Enter a non-negative value for k: ";
+		cin >> k;
+
+		if (k >= 0)
+		{
+			while (input.get(temp))
 			{
-				if (temp == reference[j])
+
+				for (int j = 0; j < reference.length(); j++)
 				{
-					shift = (j + k) % 66;
-					temp = reference[shift];
-					break;
+					if (temp == reference[j])
+					{
+						shift = (j + k) % 66;
+						temp = reference[shift];
+						break;
+					}
 				}
+
+				output << temp;
 			}
 
-			output << temp;
+			input.close();
+			output.close();
 		}
 
-		input.close();
-		output.close();
-	}
-
-	else
-	{
-		cout << "Negative value entered for k!\n";
-	}
-	
-}
-
-
-void disp_encrypted()
-{
-	ifstream encrypted_textfile;
-	encrypted_textfile.open("encrypted.txt");
-	string line = "";
-
-	if (!encrypted_textfile)
-	{
-		cout << "File not found!" << endl;
-	}
-	else
-	{
-		cout << "\t\t\t\t\t\t[Encrypted Text]\n\n";
-		while (!encrypted_textfile.eof())
+		else
 		{
-			getline(encrypted_textfile, line);
-			cout << line << endl;
+			cout << "Negative value entered for k!\n";
 		}
+
 	}
-}
 
-void disp_plain()
-{
-	ifstream plain_textfile;
-	plain_textfile.open("message.txt");
 
-	string line = "";
-
-	if (!plain_textfile)
+	void disp_encrypted()
 	{
-		cout << "File not found!" << endl;
-	}
-	else
-	{
-		cout << "\t\t\t\t\t\t[Plain Text]\n\n";
-		while (!plain_textfile.eof())
+		ifstream encrypted_textfile;
+		encrypted_textfile.open("encrypted.txt");
+		string line = "";
+
+		if (!encrypted_textfile)
 		{
-			getline(plain_textfile, line);
-			cout << line << endl;
+			cout << "File not found!" << endl;
+		}
+		else
+		{
+			cout << "\t\t\t\t\t\t[Encrypted Text]\n\n";
+			while (!encrypted_textfile.eof())
+			{
+				getline(encrypted_textfile, line);
+				cout << line << endl;
+			}
 		}
 	}
 
-}
+	void disp_plain()
+	{
+		ifstream plain_textfile;
+		plain_textfile.open("message.txt");
+
+		string line = "";
+
+		if (!plain_textfile)
+		{
+			cout << "File not found!" << endl;
+		}
+
+		else
+		{
+			cout << "\t\t\t\t\t\t[Plain Text]\n\n";
+			while (!plain_textfile.eof())
+			{
+				getline(plain_textfile, line);
+				cout << line << endl;
+			}
+		}
+
+	}
+
+};
+
 
 int main()
 {
 	bool flag = true;
 	int selector = 0;
+	encrypted encrypt_obj;
+
 
 	do
 	{
@@ -118,16 +136,16 @@ int main()
 		switch (selector)
 		{
 		case 1:
-			encrypt();
+			encrypt_obj.encrypt();
 			break;
 
 		case 2:
-			disp_encrypted();
+			encrypt_obj.disp_encrypted();
 
 			break;
 
 		case 3:
-			disp_plain();
+			encrypt_obj.disp_plain();
 			
 			break;
 
